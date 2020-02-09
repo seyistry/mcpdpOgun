67a37b69475b44b970773e75e2eef746f2134c10
tholscoa <tholscoa61@yahoo.com>
2020-02-09 22:51:39 +0100

    the printAfterReg was added.

--- mcpdp/register.php
+++ mcpdp/register.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 if (isset($_POST["submit"])) {
     include_once 'connect.php';
     // return print(json_encode($_POST));
@@ -41,8 +40,4 @@ if (isset($_POST["submit"])) {
         return print("Email already in use.");
     }
 }
-=======
-echo "Hello World!";
-echo 'This is the php file'
->>>>>>> 8bad48c1e853219aa1115926c1207fff9b9cf2a6
 ?>
